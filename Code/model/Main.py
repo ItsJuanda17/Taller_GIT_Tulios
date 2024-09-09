9c604bf153f46b8fc9037421ace9cefb2e50c198
ItsJuanda17 <1109115477@u.icesi.edu.co>
2024-09-09 17:21:54 -0500

    funcionalidad: Lista de vehiculos

---
+++ Code/model/Main.py
@@ -0,0 +1,27 @@
+
+class Main:
+    def __init__(self):
+        self.vehicles = []
+
+    def add_vehicle(self, make, model, year):
+        """
+        Añade un vehículo a la lista de vehículos.
+        
+        :param make: Marca del vehículo
+        :param model: Modelo del vehículo
+        :param year: Año del vehículo
+        """
+        vehicle = Vehicle(make, model, year)
+        self.vehicles.append(vehicle)
+        print(f"Vehículo añadido: {vehicle}")
+
+    def search_by_year(self, year):
+        """
+        Busca vehículos por año.
+        
+        :param year: Año para buscar vehículos
+        :return: Lista de vehículos que coinciden con el año dado
+        """
+        results = [vehicle for vehicle in self.vehicles if vehicle.year == year]
+        return results
+
